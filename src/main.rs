59e58a064946a637574330cf0995d8e0e3e8c4ed
Andreas Schaeffer <andreas.schaeffer@clavisit.com>
2021-01-02 15:59:10 +0100

    Multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod builder_test;
-mod glfw_test;
 mod dependency_injection_test;
+mod threading_test;
+mod glfw_test;
 
 #[macro_use]
 extern crate derive_builder;
 
 fn main() {
     builder_test::get_point_inbound();
-    glfw_test::glfw_all_tests();
+    threading_test::threading_tests();
     dependency_injection_test::dependency_injection_tests();
+    glfw_test::glfw_all_tests();
 }
