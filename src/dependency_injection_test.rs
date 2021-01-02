761e545ac8ab842e804ed772e2aa8ea0dc3eea89
Andreas Schaeffer <andreas.schaeffer@clavisit.com>
2021-01-02 04:18:19 +0100

    Dependency injection

---
+++ src/dependency_injection_test.rs
@@ -0,0 +1,35 @@
+extern crate config;
+extern crate serde;
+extern crate waiter_di;
+
+use std::rc::Rc;
+
+use waiter_di::*;
+
+trait Interface: Send {
+    fn demo(&self);
+}
+
+#[component]
+struct InterfaceImpl {}
+
+#[provides]
+impl Interface for InterfaceImpl {
+    fn demo(&self) {
+        println!("Dependency");
+    }
+}
+
+#[component]
+struct SomeComp {
+    #[prop("i32_prop")] prop: i32,
+    interface: Rc<dyn Interface>
+}
+
+
+pub fn dependency_injection_tests() {
+    let mut container = Container::<profiles::Default>::new();
+    let component = Provider::<SomeComp>::get(&mut container);
+
+    component.interface.demo();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod builder_test;
 mod glfw_test;
+mod dependency_injection_test;
 
 #[macro_use]
 extern crate derive_builder;
@@ -7,4 +8,5 @@ extern crate derive_builder;
 fn main() {
     builder_test::get_point_inbound();
     glfw_test::glfw_all_tests();
+    dependency_injection_test::dependency_injection_tests();
 }
