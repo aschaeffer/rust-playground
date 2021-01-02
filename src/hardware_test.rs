df74c263433dd4e17592deede8c22f62d2eec054
Andreas Schaeffer <andreas.schaeffer@clavisit.com>
2021-01-02 20:30:21 +0100

    Remove unused imports

--- src/hardware_test.rs
+++ src/hardware_test.rs
@@ -1,5 +1,3 @@
-use num_cpus::*;
-
 pub fn hardware_tests() {
     println!("Number of logical cores is {}", num_cpus::get());
     println!("Number of physical cores is {}", num_cpus::get_physical());
